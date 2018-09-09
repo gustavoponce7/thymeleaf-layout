1c0bb6c1014f0d72f711c64e2408a78acda04294
Gustavo Ponce <gpch@MacBook-Pro.Dlink>
2018-09-09 19:30:55 +0300

    first commit

---
+++ thymeleaf-layout/src/main/java/com/gpch/thymeleaflayout/ThymeleafLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.gpch.thymeleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ThymeleafLayoutApplication.class, args);
+    }
+}
